3b46b19302c93e9de1662c953d0813ec9394f5d7
N/A <ruben.wood1@gmail.com>
2021-03-22 15:37:16 +0000

    Updated CORS policy

--- app/Startup.cs
+++ app/Startup.cs
@@ -26,6 +26,13 @@ namespace WebApp_OpenIDConnect_DotNet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                builder => builder.AllowAnyOrigin());
+            });
+            
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
