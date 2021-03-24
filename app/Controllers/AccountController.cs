1cf07e113530f85453d94322002ce23fb0bf2df6
N/A <ruben.wood1@gmail.com>
2021-03-24 11:12:14 +0000

    commented old redirect url

--- app/Controllers/AccountController.cs
+++ app/Controllers/AccountController.cs
@@ -42,7 +42,7 @@ namespace WebApp_OpenIDConnect_DotNet.Controllers
 
             var redirect = "https://google.com/" + Request.Form["id_token"];
 
-            var redirectUrl = Url.Action(nameof(HomeController.Index), "Home");
+            //var redirectUrl = Url.Action(nameof(HomeController.Index), "Home");
             return Challenge(
                 new AuthenticationProperties { RedirectUri = redirect },
                 OpenIdConnectDefaults.AuthenticationScheme);
